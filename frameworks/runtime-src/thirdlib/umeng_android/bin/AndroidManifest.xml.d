7257e3853707137f48a2a057f2c5f5c3c1457170
caoguoping <caoguoping100@163.com>
2017-09-29 00:48:33 +0800

    delete libs

--- frameworks/runtime-src/thirdlib/umeng_android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\RYNewGame\556Mobile\frameworks\runtime-src\thirdlib\umeng_android\bin\AndroidManifest.xml : \
-E:\RYNewGame\556Mobile\frameworks\runtime-src\thirdlib\umeng_android\AndroidManifest.xml \
